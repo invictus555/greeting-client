22468763d07fd1868a7ce88b8ee6c9b11f5a51c1
invictus555 <38687666+invictus555@users.noreply.github.com>
2022-09-25 22:28:18 +0800

    web版client

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module greeting-client
 go 1.16
 
 require (
+	github.com/gorilla/mux v1.8.0
 	github.com/invictus555/greeting v0.0.1
+	github.com/pkg/errors v0.9.1
 	google.golang.org/grpc v1.49.0
 	google.golang.org/protobuf v1.28.1
 )
--- main.go
+++ main.go
@@ -4,52 +4,79 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/gorilla/mux"
 	. "github.com/invictus555/greeting/api/greeting"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"log"
+	"net/http"
 )
 
 var (
-	port *int    // greetingServer的监听端口
-	addr *string // greetingServer的监听地址
+	httpPort       *int             // http server服务端口
+	greetingPort   *int             // greetingServer的监听端口
+	greetingAddr   *string          // greetingServer的监听地址
+	greetingServer *grpc.ClientConn // greeetingServer conn
 )
 
 func init() {
 	// 获取GreetingServer的监听端口与监听地址
-	port = flag.Int("port", 50050, "the port of greeting server")
-	addr = flag.String("svrAddr", "localhost", "the address of greeting server")
+	httpPort = flag.Int("httpServerPort", 8080, "the port of http server")
+	greetingPort = flag.Int("greetingServerPort", 50050, "the port of greeting server")
+	greetingAddr = flag.String("greetingServerAddress", "localhost", "the address of greeting server")
 	flag.Parse() // 解析命令行参数
+
+	greetingServer = NewGreetingServer(fmt.Sprintf("%s:%d", *greetingAddr, *greetingPort))
 }
 
-func main() {
-	// 连接gRPC服务器
-	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", *addr, *port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+func NewGreetingServer(addr string) *grpc.ClientConn {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials())) // 连接gRPC服务器
 	if err != nil {
-		fmt.Printf("Failed to dial to: " + fmt.Sprintf("%s:%d", *addr, *port))
+		fmt.Printf("Failed to dial to: " + addr)
 	}
 
 	if conn == nil {
 		panic("Failed to get connection from the server")
 	}
 
-	defer conn.Close()
-	c := NewGreetingClient(conn) // 初始化客户端
+	return conn
+}
 
-	// 调用方法
+func goodBye(w http.ResponseWriter, r *http.Request) {
+	c := NewGreetingClient(greetingServer) // 初始化客户端
+
+	empty := new(emptypb.Empty)
+	resp, err := c.SayBye(context.Background(), empty)
+	if err != nil {
+		fmt.Printf("Fail to call SayBye method, err: %v", err)
+	}
+
+	fmt.Println(resp.Message)    // 打印消息
+	w.WriteHeader(http.StatusOK) // 设置响应状态码为 200
+	fmt.Fprintf(w, resp.Message) // 发送响应到客户端
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	c := NewGreetingClient(greetingServer) // 初始化客户端
+
 	reqBody := new(HelloRequest)
 	reqBody.Name = "gRPC Server"
-	r, err := c.SayHello(context.Background(), reqBody)
+	resp, err := c.SayHello(context.Background(), reqBody) // 调用方法
 	if err != nil {
 		fmt.Printf("Fail to call SayHello method, err: %v", err)
 	}
 
-	fmt.Println(r.Message)
+	fmt.Println(resp.Message)    // 打印消息
+	w.WriteHeader(http.StatusOK) // 设置响应状态码为 200
+	fmt.Fprintf(w, resp.Message) // 发送响应到客户端
+}
 
-	empty := new(emptypb.Empty)
-	bye, err := c.SayBye(context.Background(), empty)
-	if err != nil {
-		fmt.Printf("Fail to call SayBye method, err: %v", err)
-	}
-	fmt.Println(bye.Message)
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/bye", goodBye).Methods(http.MethodGet)
+	r.HandleFunc("/hello", sayHello).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *httpPort), r))
+	greetingServer.Close()
 }
